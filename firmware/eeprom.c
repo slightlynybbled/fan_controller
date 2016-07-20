0caa66dbc4a8826665e155769c808dfbfbe9ae8c
Jason R. Jones <slightlynybbled@gmail.com>
2016-07-20 08:59:55 -0400

    Fixed an error in which the address wasn't even as the device requires

--- firmware/eeprom.c
+++ firmware/eeprom.c
@@ -1,14 +1,14 @@
 #include "eeprom.h"
 #include <xc.h>
 
-uint16_t __attribute__((space(eedata))) eedata = 0xffff;
+uint16_t __attribute__((space(eedata))) eedata[__EEDATA_LENGTH >> 1];
 
 void EEPROM_erase(uint16_t address){
     NVMCON = 0x4058;
     
     /* Set up a pointer to the EEPROM location to be written */
-    TBLPAG = __builtin_tblpage(&eedata);
-    uint16_t offset = __builtin_tbloffset(&eedata) + address;
+    TBLPAG = __builtin_tblpage(eedata);
+    uint16_t offset = __builtin_tbloffset(eedata) + (address << 1);
     __builtin_tblwtl(offset, offset);
     
     asm volatile ("disi #5");
@@ -22,8 +22,8 @@ void EEPROM_write(uint16_t address, uint16_t value){
     NVMCON = 0x4004;
     
     /* Set up a pointer to the EEPROM location to be written */
-    TBLPAG = __builtin_tblpage(&eedata);
-    uint16_t offset = __builtin_tbloffset(&eedata) + address;
+    TBLPAG = __builtin_tblpage(eedata);
+    uint16_t offset = __builtin_tbloffset(eedata) + (address << 1);
     __builtin_tblwtl(offset, value);
     
     asm volatile ("disi #5");
@@ -35,8 +35,8 @@ uint16_t EEPROM_read(uint16_t address){
     uint16_t value;
     
     /* Set up a pointer to the EEPROM location to be read */
-    TBLPAG = __builtin_tblpage(&eedata);
-    uint16_t offset = __builtin_tbloffset(&eedata) + address;
+    TBLPAG = __builtin_tblpage(eedata);
+    uint16_t offset = __builtin_tbloffset(eedata) + (address << 1);
     
     value = __builtin_tblrdl(offset);
     
